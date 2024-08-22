c6f84500f9212bd5f8c2fe6a67910f1fab6fef50
Bartek <bartek.hometowski@gmail.com>
2024-08-22 22:38:37 +0200

    add StringBuilder to Emitter

--- src/main/scala/Emitter.scala
+++ src/main/scala/Emitter.scala
@@ -3,21 +3,20 @@ import java.io.{File, PrintWriter}
 // Keeps track of the generated code and outputs it
 class Emitter(path: String) {
 
-  var header = ""
-  var code = ""
+  val header = StringBuilder("")
+  val code = StringBuilder("")
 
-  def emit(c: String) = code += c
+  def emit(c: String) = code ++= c
 
-  def emitLine(c: String) = code += c + '\n'
+  def emitLine(c: String) = code ++= c += '\n'
 
-  def headerLine(c: String) = header += c + '\n'
+  def headerLine(c: String) = header ++= c += '\n'
 
   def writeFile = {
-//    val file = new File(path)
     val file = new File(path)
     file.createNewFile()
     val printWriter = new PrintWriter(file)
-    printWriter.write(header + code)
+    printWriter.write((header ++= code).toString())
     printWriter.close
   }
 
